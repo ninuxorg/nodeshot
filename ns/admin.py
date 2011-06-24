5af418acf82009fe49146fa31ae5f7f0d378f0e1
Federico Capoano <federico@nemesisdesign.net>
2011-06-24 22:45:19 +0200

    Added some backend controls for Devices

--- ns/admin.py
+++ ns/admin.py
@@ -8,7 +8,13 @@ class NodeAdmin(admin.ModelAdmin):
     save_on_top = True
 admin.site.register(Node, NodeAdmin)
 
+class DeviceAdmin(admin.ModelAdmin):
+    list_display  = ('name', 'node', 'type', 'added', 'updated')
+    list_filter   = ('added', 'updated', 'node')
+    search_fields = ('name', 'type')
+    save_on_top = True
+admin.site.register(Device, DeviceAdmin)
+
 admin.site.register(Interface)
 admin.site.register(HNAv4)
 admin.site.register(Link)
-admin.site.register(Device)
