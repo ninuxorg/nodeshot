8b4d920b21889c7e83e38d099c9ebf30aeb89f67
OrazioPirataDelloSpazio <ziducaixao@autistici.org>
2011-07-17 14:32:06 +0000

    added surprise ;-)

--- media/js/gmap.js
+++ media/js/gmap.js
@@ -280,6 +280,20 @@ var nodeshotRemoveMask = function(){
     });
 }
 
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
+    $.getScript('http://www.cornify.com/js/cornify.js',function(){
+      cornify_add();
+      $(document).keydown(cornify_add);
+    });          
+  }
+});
+
+
 function initialize() {
     initialize_map();
     // Jquery autocomplete
