93bbf652f0f61ae0b79466be36a33366fc83e955
OrazioPirataDelloSpazio <ziducaixao@autistici.org>
2011-10-04 03:53:34 +0000

    comments added to the bash script

--- nodeshot/scripts/update_links.sh
+++ nodeshot/scripts/update_links.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Just put this file in crontab to execute all the scripts periodically
+# I use python virtual environment (venv) but you couldn't have such need. In this case just comment the folling line.
 source ../../venv/bin/activate
+#get ETX of all the links
 DJANGO_SETTINGS_MODULE=settings ./read_olsr_topology.py
+#poll nodes to get or update information through snmp
 DJANGO_SETTINGS_MODULE=settings ./snmp.py
