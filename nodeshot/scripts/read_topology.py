fbacf5e890549437b290663ada5bbb290c791ca8
OrazioPirataDelloSpazio <ziducaixao@autistici.org>
2011-10-17 05:40:34 +0000

    merged read topology for olsr and batman. Added link deletion

--- nodeshot/scripts/read_topology.py
+++ nodeshot/scripts/read_topology.py
@@ -14,6 +14,7 @@ __builtins__.IS_SCRIPT = True
 from nodeshot.models import *
 from django.db.models import Q
 
+TOPOLOGY_URLS=["http://eigenlab.org/battxtinfo.php", "http://coppermine.eigenlab.org/battxtinfo.php"]
 
 # Take all ips of a node with MID olsr, for any couple of node composing a link, so [ip1A,ip2A,ip3A] <--> [ip1B,ip2B]
 # query db for existing link from one ip of A to one ip of B (break ties randomly). Else create a new one
@@ -141,7 +142,6 @@ if __name__ == "__main__":
     tp.parse()
     tp.process()
     old_links = dict([ (l.id, False) for l in Link.objects.all()])
-    exit  
     print tp.linkdict
     for v in tp.linkdict.values():
         ipsA, ipsB, etx = v
@@ -172,8 +172,43 @@ if __name__ == "__main__":
                             l = Link(from_interface = fi.get(), to_interface = to.get(), etx = etx).save()
                             print "Saved new link: %s" % l
                             found = True
+    for topology_url in TOPOLOGY_URLS:
+        topologylines = urllib2.urlopen(topology_url).readlines()
+        for line in topologylines:
+            row_elements = line.split()
+            if len(row_elements) == 5:
+                # this is a good row
+                macA, macB, cost = row_elements[0], row_elements[1], row_elements[4]
+                print macA + "<-->" + macB +" : " + cost
+                saved_links =  Link.objects.filter(Q(from_interface__mac_address=macA , to_interface__mac_address=macB ) |  Q(from_interface__mac_address=macB , to_interface__mac_address=macA ))
+                # I think is better to use len(saved_links) than saved_links.count() cos it results in less hits to the database
+                link_count = len(saved_links)
+                if 0 < link_count < 2:
+                    l = saved_links[0]
+                    # if a link already exists, just update
+                    l.etx = cost
+                    old_links[l.id] = True
+                    l.save()
+                    print "Updated etx to %s for link %s" % (cost, l)
+                elif link_count > 1:
+                    print "Anomaly: 2 links retrieved."
+                elif link_count < 1:
+                    # otherwise create the new link
+                    fi = Interface.objects.filter(mac_address = macA)
+                    to = Interface.objects.filter(mac_address = macB)
+                    fi_count = len(fi)
+                    to_count = len(to)
+                    if 0 < fi_count < 2 and 0 < to_count < 2:
+                        if fi.get().device.node != to.get().device.node:
+                            # create a link if the neighbors are NOT on the same node
+                            try:
+                                l = Link(from_interface = fi.get(), to_interface = to.get(), etx = cost).save()
+                                print 'Saved new link %s' % l
+                            except:
+                                print 'Error while saving link from %s to %s' % (fi.device.node, to.device.node)
+
     for l,v in old_links.iteritems() :
         if not v:
-            #Link.objects.get(id=l).delete()
+            Link.objects.get(id=l).delete()
             print "Deleted link %d" % l
         
--- nodeshot/scripts/update_links.sh
+++ nodeshot/scripts/update_links.sh
@@ -3,7 +3,7 @@
 # We use python virtual environment (venv) but you couldn't have such need. In that case just comment the folling line.
 #source ../../venv/bin/activate
 #get ETX of all the links
-DJANGO_SETTINGS_MODULE=settings /home/ninux/nodeshot/venv/bin/python /home/ninux/nodeshot/nodeshot/scripts/read_olsr_topology.py
-DJANGO_SETTINGS_MODULE=settings /home/ninux/nodeshot/venv/bin/python /home/ninux/nodeshot/nodeshot/scripts/read_batman_topology.py
+DJANGO_SETTINGS_MODULE=settings /home/ninux/nodeshot/venv/bin/python /home/ninux/nodeshot/nodeshot/scripts/read_topology.py
+#DJANGO_SETTINGS_MODULE=settings /home/ninux/nodeshot/venv/bin/python /home/ninux/nodeshot/nodeshot/scripts/read_batman_topology.py
 #poll nodes to get or update information through snmp
 DJANGO_SETTINGS_MODULE=settings /home/ninux/nodeshot/venv/bin/python /home/ninux/nodeshot/nodeshot/scripts/update_db.py
