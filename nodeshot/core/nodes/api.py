3969f50fd702970df6b79cd9c1bc23f018320eb9
nemesisdesign <federico.capoano@teletu.it>
2012-09-11 20:34:16 +0200

    RESTful api started

---
+++ nodeshot/core/nodes/api.py
@@ -0,0 +1,28 @@
+from tastypie.resources import ModelResource
+from models import Node
+
+class NodeResource(ModelResource):
+    class Meta:
+        queryset = Node.objects.all()
+        resource_name = 'nodes'
+        excludes = ['user', 'description', 'notes', 'added', 'updated', 'access_level']
+    
+    def dehydrate(self, bundle):
+        # If they're requesting their own record, add in their email address.
+        if self:
+            # Note that there isn't an ``email`` field on the ``Resource``.
+            # By this time, it doesn't matter, as the built data will no
+            # longer be checked against the fields on the ``Resource``.
+            bundle.data.pop('resource_uri')
+            
+            if self.get_resource_uri(bundle) == bundle.request.path:
+                bundle.data['user'] = bundle.obj.user.username
+                bundle.data['description'] = bundle.obj.description
+                bundle.data['added'] = bundle.obj.added
+    
+            #if self.get_resource_uri(bundle) != bundle.request.path:
+            #    print "Not Detail - Could be list or reverse relationship."
+    
+            return bundle
+    
+        return bundle
--- projects/ninux/ninux/urls.py
+++ projects/ninux/ninux/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^api/', include('nodeshot.core.api.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
