b70383ba832cbce85ec6ded7df5fcb0eb503d096
nemesisdesign <federico.capoano@teletu.it>
2012-09-13 16:18:28 +0200

    Added "get_zone_nodes" view: /zones/<slug>/nodes/ Tidy up

--- nodeshot/core/zones/api.py
+++ nodeshot/core/zones/api.py
@@ -1,27 +1,36 @@
+from django.conf.urls import url
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from tastypie.resources import ModelResource
+from tastypie.http import HttpGone, HttpMultipleChoices
 from models import Zone, ZoneExternal
+from nodeshot.core.nodes.api import NodeResource
 
 class ZoneResource(ModelResource):
+    
     class Meta:
         queryset = Zone.objects.all().select_related()
         resource_name = 'zones'
+        include_resource_uri = False
         excludes = ['added', 'updated']
     
-    def dehydrate(self, bundle):
-        # If they're requesting their own record, add in their email address.
-        if self:
-            # Note that there isn't an ``email`` field on the ``Resource``.
-            # By this time, it doesn't matter, as the built data will no
-            # longer be checked against the fields on the ``Resource``.
-            bundle.data.pop('resource_uri')
-            
-            #if self.get_resource_uri(bundle) == bundle.request.path:
-            #    bundle.data['user'] = bundle.obj.user.username
-            #    bundle.data['description'] = bundle.obj.description
-            #    bundle.data['added'] = bundle.obj.added
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w\d_.-]+)/nodes/$" % self._meta.resource_name, self.wrap_view('get_zone_nodes'), name="api_get_zone_nodes"),
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
     
-            #if self.get_resource_uri(bundle) != bundle.request.path:
-            #    print "Not Detail - Could be list or reverse relationship."
-    
-        return bundle
+    def get_zone_nodes(self, request, **kwargs):
+        try:
+            obj = self.cached_obj_get(request=request, **self.remove_api_resource_names(kwargs))
+        except ObjectDoesNotExist:
+            return HttpGone()
+        except MultipleObjectsReturned:
+            return HttpMultipleChoices(_('More than one zone is found at this URI.'))
+
+        filters = request.GET.copy()
+        filters['zone'] = obj.pk
+        request.GET = filters
+
+        child_resource = NodeResource()
+        return child_resource.get_list(request)
 
