d0f6d8682aaf541566839b678e80f0af72a2dddf
nemesisdesign <federico.capoano@teletu.it>
2012-09-14 20:49:43 +0200

    Added images of a node api URL: /nodes/<node_slug>/images/ Plus minor tweaks

--- nodeshot/core/nodes/resources.py
+++ nodeshot/core/nodes/resources.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import url
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from tastypie.http import HttpNotFound
 from tastypie.resources import ModelResource, ALL
 from tastypie import fields
 from models import Node, Image
@@ -11,7 +13,7 @@ from models import Node, Image
 #        return output   
 
 class NodeResource(ModelResource):
-    zone = fields.ForeignKey('nodeshot.core.zones.api.ZoneResource', 'zone')
+    zone = fields.ForeignKey('nodeshot.core.zones.resources.ZoneResource', 'zone')
     
     class Meta:
         queryset = Node.objects.all().select_related()
@@ -33,29 +35,53 @@ class NodeResource(ModelResource):
         ]
     
     def dehydrate(self, bundle):
-        # If they're requesting their own record, add in their email address.
-        if self:
-            # Note that there isn't an ``email`` field on the ``Resource``.
-            # By this time, it doesn't matter, as the built data will no
-            # longer be checked against the fields on the ``Resource``.
-            #bundle.data.pop('resource_uri')
-            
-            if self.get_resource_uri(bundle) == bundle.request.path:
-                bundle.data['user'] = bundle.obj.user.username
-                bundle.data['description'] = bundle.obj.description
-                bundle.data['added'] = bundle.obj.added
-            
-            bundle.data['zone'] = bundle.obj.zone.slug
-    
-            #if self.get_resource_uri(bundle) != bundle.request.path:
-            #    print "Not Detail - Could be list or reverse relationship."
+        # if list view
+        if self.get_resource_uri(bundle) == bundle.request.path:
+            bundle.data['user'] = bundle.obj.user.username
+            bundle.data['description'] = bundle.obj.description
+            bundle.data['added'] = bundle.obj.added
+        
+        # zone slug instead of URI to save bandwidth
+        bundle.data['zone'] = bundle.obj.zone.slug
     
         return bundle
 
 class ImageResource(ModelResource):
+    node = fields.ForeignKey('nodeshot.core.nodes.api.NodeResource', 'node')
+    
+    def override_urls(self):
+        return [
+            url(r"^nodes/(?P<node_slug>[\w\d_.-]+)/(?P<resource_name>%s)/$" % self._meta.resource_name, self.wrap_view('get_node_images'), name="api_get_node_images"),
+        ]
+    
     class Meta:
         queryset = Image.objects.all()
         resource_name = 'images'
         include_resource_uri = False
-        excludes = ['added', 'updated', 'access_level']
+        limit = 0
+        excludes = ['added', 'updated', 'access_level', 'id']
+        
+        filtering = {
+            'node': ALL,
+        }
 
+    def get_node_images(self, request, **kwargs):
+        """ images of a node """
+        slug = kwargs.pop('node_slug')
+        
+        try:
+            node = Node.objects.get(slug=slug)
+        except ObjectDoesNotExist:
+            return HttpNotFound()
+            
+        filters = request.GET.copy()
+        filters['node'] = node.pk
+        request.GET = filters
+
+        return self.get_list(request)
+
+    def dehydrate(self, bundle):
+        # node slug instead of URI to save space
+        bundle.data['node'] = bundle.obj.node.slug
+        
+        return bundle
