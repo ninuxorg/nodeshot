3b26031109bbad0c3b50a2ef9b1e5c54a3eca755
nemesisdesign <federico.capoano@teletu.it>
2012-10-01 18:31:08 +0200

    default ZONE_TIME, ZONE_ZOOM plus minor improvements to zones app

--- nodeshot/core/zones/admin.py
+++ nodeshot/core/zones/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from nodeshot.core.base.admin import BaseAdmin
-from models import Zone, ZoneExternal
+from django.conf import settings
+from nodeshot.core.base.admin import BaseAdmin, BaseStackedInline
+from models import Zone, ZonePoint, ZoneExternal
+
+class ZonePointInline(admin.StackedInline):
+    model = ZonePoint
+    extra = 0
+    
+    if 'grappelli' in settings.INSTALLED_APPS:
+        classes = ('grp-collapse grp-open', )
 
 class ZoneAdmin(BaseAdmin):
     list_display = ('name', 'description', 'is_external', 'mantainers', 'email', 'time_zone')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter   = ('is_external',)
+    inlines = [ZonePointInline]
 
 admin.site.register(Zone, ZoneAdmin)
-admin.site.register(ZoneExternal, ZoneAdmin)
+#admin.site.register(ZoneExternal, ZoneAdmin)
--- nodeshot/core/zones/models/zone.py
+++ nodeshot/core/zones/models/zone.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from nodeshot.core.base.models import BaseDate
 from nodeshot.core.base.choices import TIME_ZONES, MAP_ZOOM
 
 class Zone(BaseDate):
     name = models.CharField(_('name'), max_length=50, unique=True)
     parent = models.ForeignKey('Zone', blank=True, null=True)
-    time_zone = models.CharField(_('time zone'), max_length=8, choices=TIME_ZONES) # TODO: default
+    time_zone = models.CharField(_('time zone'), max_length=8, choices=TIME_ZONES, default=settings.NODESHOT['DEFAULTS']['ZONE_TIME'])
     description = models.CharField(_('description'), max_length=250, blank=True, null=True)
     slug = models.SlugField(max_length=50, db_index=True, unique=True)
     lat = models.FloatField(_('center latitude'))
     lng = models.FloatField(_('center longitude'))
-    zoom = models.SmallIntegerField(_('default zoom level'), choices=MAP_ZOOM, default=12) #TODO: default configurable
-    is_external = models.BooleanField()
+    zoom = models.SmallIntegerField(_('default zoom level'), choices=MAP_ZOOM, default=settings.NODESHOT['DEFAULTS']['ZONE_ZOOM'])
     mantainers = models.CharField(_('mantainers'), help_text=_('Organizations like Ninux, Freifunk or people like Alice and Bob'), max_length=255)
     email = models.EmailField(_('email'), help_text=_('possibly an email address that delivers messages to all the active participants'))
     website = models.URLField(_('Website'), blank=True, null=True)
+    is_external = models.BooleanField()
     
     def __unicode__(self):
         return '%s' % self.name
