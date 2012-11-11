535a70842c3b35fd14ca6a350e74b2c9ee91208c
nemesisdesign <federico.capoano@teletu.it>
2012-11-11 21:02:40 +0100

    improved BaseSlugResource to overwrite 'get_resource_uri'

--- nodeshot/core/base/resources.py
+++ nodeshot/core/base/resources.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 from tastypie.resources import ModelResource
+from tastypie.bundle import Bundle
 
 class BaseSlugResource(ModelResource):
-    """ Base Model Resource which contains 'get_slug_detail_uri' method """
+    """ Base Model Resource which contains 'get_slug_detail_uri' method and overrides get_resource_uri """
     
     class Meta:
         abstract = True
@@ -15,3 +16,18 @@ class BaseSlugResource(ModelResource):
         kwargs['slug'] = bundle.obj.slug
         # return generated uri
         return reverse('api_dispatch_detail', kwargs=kwargs)
+    
+    def get_resource_uri(self, bundle_or_obj=None, url_name='api_dispatch_list'):
+        """
+        Custom get_resource_uri which returns a slug based detail uri when necessary
+        """
+        if bundle_or_obj is not None:
+            url_name = 'api_dispatch_detail'
+            
+            if isinstance(bundle_or_obj, Bundle):
+                return self.get_slug_detail_uri(bundle_or_obj)
+
+        try:
+            return self._build_reverse_url(url_name, kwargs=self.resource_uri_kwargs(bundle_or_obj))
+        except NoReverseMatch:
+            return ''
