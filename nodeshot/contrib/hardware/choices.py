18cadb96e0ccb9368f02690598af220b7700f0cd
nemesisdesign <federico.capoano@teletu.it>
2012-12-23 19:39:04 +0100

    start moving choices in their respective apps each app should be as much indipendent as possible from other apps, included the base app. If some code is specific of a certain app, it should reside in its app folder and nowhere else.

---
+++ nodeshot/contrib/hardware/choices.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
+from nodeshot.core.base.utils import choicify
+
+
+POLARIZATIONS = {
+    'horizonal': 1,
+    'vertical': 2,
+    'circular': 3,
+    'linear': 4,
+    'dual_linear': 5
+}
+
+POLARIZATION_CHOICES = choicify(POLARIZATIONS)
