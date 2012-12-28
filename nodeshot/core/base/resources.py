b7a4c301cf058f0fcec8e8ebe0a2634c02e69f08
nemesisdesign <federico.capoano@teletu.it>
2012-12-29 00:00:37 +0100

    Added extended TastyPie Resource: "BaseExtraResource" to nodeshot.core.base.resources Allows an extra Meta class attribute that will be included in the get_list view

--- nodeshot/core/base/resources.py
+++ nodeshot/core/base/resources.py
@@ -2,6 +2,40 @@ from django.core.urlresolvers import reverse
 from tastypie.resources import ModelResource
 from tastypie.bundle import Bundle
 
+
+class BaseExtraResource(ModelResource):
+    """ Tastypie extended ModelResource that can take an extra param in the meta class that will add some stuff to the list output :-) """
+    
+    def get_list(self, request, **kwargs):
+        """
+        Returns a serialized list of resources.
+
+        Calls ``obj_get_list`` to provide the data, then handles that result
+        set and serializes it.
+
+        Should return a HttpResponse (200 OK).
+        """
+        # TODO: Uncached for now. Invalidation that works for everyone may be
+        #       impossible.
+        objects = self.obj_get_list(request=request, **self.remove_api_resource_names(kwargs))
+        sorted_objects = self.apply_sorting(objects, options=request.GET)
+
+        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=self.get_resource_uri(), limit=self._meta.limit, max_limit=self._meta.max_limit, collection_name=self._meta.collection_name)
+        to_be_serialized = paginator.page()
+
+        # Dehydrate the bundles in preparation for serialization.
+        bundles = [self.build_bundle(obj=obj, request=request) for obj in to_be_serialized[self._meta.collection_name]]
+        
+        try:
+            for key, value in self._meta.extra.items():
+                to_be_serialized[key] = value
+        except AttributeError:
+            pass
+        
+        to_be_serialized[self._meta.collection_name] = [self.full_dehydrate(bundle) for bundle in bundles]
+        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
+        return self.create_response(request, to_be_serialized)
+
 class BaseSlugResource(ModelResource):
     """ Base Model Resource which contains 'get_slug_detail_uri' method and overrides get_resource_uri """
     
