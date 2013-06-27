30bb12c5a45a8ebf6070f0e03cdc0749085af4bc
nemesisdesign <federico.capoano@teletu.it>
2013-06-27 11:12:29 +0200

    Custom User Admin improvement, email address shown as inline in case PROFILE_EMAIL_CONFIRMATION is True

--- nodeshot/community/profiles/admin.py
+++ nodeshot/community/profiles/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+from django.contrib.auth.forms import UserChangeForm as BaseChangeForm
+from django.contrib.auth.forms import UserCreationForm as BaseCreationForm
+from django.contrib.auth.forms import AdminPasswordChangeForm as BasePasswordChangeForm
 
 from nodeshot.core.base.admin import BaseStackedInline
+from .models import SocialLink, Profile
 
-from .models import SocialLink, Profile#, EmailNotification
-from .forms import *
+PROFILE_EMAIL_CONFIRMATION = settings.NODESHOT['SETTINGS'].get('PROFILE_EMAIL_CONFIRMATION', True)
+
+
+# --- User management forms --- #
+
+
+class UserChangeForm(BaseChangeForm):
+    class Meta:
+        model = Profile
+
+
+class UserCreationForm(BaseCreationForm):
+    class Meta:
+        model = Profile
+
+
+class AdminPasswordChangeForm(BasePasswordChangeForm):
+    class Meta:
+        model = Profile
+
+
+# --- Admin --- #
 
 
 class ProfileSocialLinksInline(BaseStackedInline):
@@ -24,16 +50,27 @@ class UserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
     
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': (
+    fieldsets = [
+        [None, {'fields': ('username', 'password')}],
+        [_('Personal info'), {'fields': [
             'first_name', 'last_name', 'email',
             'birth_date', 'address', 'city', 'country'
-        )}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
+        ]}],
+        [_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}],
+        [_('Important dates'), {'fields': ('last_login', 'date_joined')}],
+    ]
+
+
+if PROFILE_EMAIL_CONFIRMATION:    
+    from emailconfirmation.models import EmailAddress
+    
+    class EmailAddressInline(admin.StackedInline):
+        model = EmailAddress
+        extra = 0
+    
+    UserAdmin.inlines = [EmailAddressInline] + UserAdmin.inlines
+    UserAdmin.fieldsets[1][1]['fields'].remove('email')
 
 
 admin.site.register(Profile, UserAdmin)
