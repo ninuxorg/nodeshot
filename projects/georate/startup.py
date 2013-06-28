102529b1fe5f5850d08f8d3774b1246727a01858
stefano <stefano@stefano.svil>
2013-06-28 18:12:11 +0200

    Clean method commented on participation models

--- projects/georate/startup.py
+++ projects/georate/startup.py
@@ -8,6 +8,7 @@ try:
     User = get_user_model()
     from nodeshot.core.nodes.models import Node
     from nodeshot.core.layers.models import Layer
+    from nodeshot.community.participation.models import Comment, Rating, Vote
     from nodeshot.community.mailing.models import Outward, Inward
 except ImportError as e:
     raise Exception('wrong imports!\n\n%s' % e.message)
