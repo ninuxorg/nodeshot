f415a1574d6166ee4c9f283188a900442f05b277
stefano <stefano@stefano.svil>
2013-07-31 13:10:55 +0200

    Geojson API for layers

--- nodeshot/core/layers/urls.py
+++ nodeshot/core/layers/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('nodeshot.core.layers.views',
     url(r'^/layers/(?P<slug>[-\w]+)/$', 'layer_detail', name='api_layer_detail'),
     url(r'^/layers/(?P<slug>[-\w]+)/nodes/$', 'nodes_list', name='api_layer_nodes_list'),
     url(r'^/layers/(?P<slug>[-\w]+)/geojson/$', 'nodes_geojson_list', name='api_layer_nodes_geojson'),
+    url(r'^/layers-geojson/$', 'layers_geojson_list', name='api_layer_geojson'),
 )
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
