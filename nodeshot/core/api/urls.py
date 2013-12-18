b99b41259f67c1a6f2e5f53c0234256d2696635b
nemesisdesign <nemesis@ninux.org>
2013-12-18 13:58:45 +0100

    API ENABLED modules fail silently if module not in INSTALLED_APPS

--- nodeshot/core/api/urls.py
+++ nodeshot/core/api/urls.py
@@ -15,10 +15,10 @@ if 'rest_framework_swagger' in settings.INSTALLED_APPS:
 # loop over all the strings listed in settings.NODESHOT['API']['APPS_ENABLED]
 for app_path in settings.NODESHOT['API']['APPS_ENABLED']:
     
-    # ensure enabled API module is listed in INSTALLED_APPS
+    # enable only API modules listed in INSTALLED_APPS
     if app_path not in settings.INSTALLED_APPS:
-        raise ImproperlyConfigured("""%s is listed in settings.NODESHOT['API']['APPS_ENABLED']
-                                   but is not listed in settings.INSTALLED_APPS. Please fix.""" % app_path)
+        # fail silently
+        continue
     
     # determine import path for url patterns
     module_path = '%s.urls' % app_path
