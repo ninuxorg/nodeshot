fa81cc3785a3a72377f2ad735ccbc5fac71f40fd
nemesisdesign <nemesis@ninux.org>
2014-03-04 18:20:47 +0100

    Open311 settings.example.py indentation cleanup

--- projects/ninux/ninux/settings.example.py
+++ projects/ninux/ninux/settings.example.py
@@ -532,28 +532,28 @@ NODESHOT = {
         ('netengine.backends.snmp.AirOS', 'AirOS (SNMP)'),
     ],
     'OPEN311': {
-        #Metadata for service discovery
+        # Metadata for service discovery
         'DISCOVERY': {
             'changeset':'2014-02-03 14:18',
             'contact':'email or phone number for assistance',
             'key_service':'URL for api_key requests',
             'endpoints':[
-              {
-                'specification':'http://wiki.open311.org/GeoReport_v2',
-                'url':'Public URL of your endpoint',
-                'changeset':'2014-02-03 09:01',
-                'type':'production',
-                'formats':[
-                  'application/json'
-                ]
-              },
+                {
+                    'specification':'http://wiki.open311.org/GeoReport_v2',
+                    'url':'Public URL of your endpoint',
+                    'changeset':'2014-02-03 09:01',
+                    'type':'production',
+                    'formats':[
+                        'application/json'
+                    ]
+                },
               
             ]
-          },
-        #Do not change this unless you want to redefine Open311 service definitions
+        },
+        # Do not change this unless you want to redefine Open311 service definitions
         'METADATA': 'true',
         'TYPE': 'realtime',
-        #Change the following, according to the statuses you have configured in your model
+        # Change the following, according to the statuses you have configured in your model
         #'STATUS' : {
         #    'Potential' : 'open',
         #    'Planned' : 'open',
