cd58ceb959493533014229537a7c73da2a6b3985
Federico Capoano <nemesis@ninux.org>
2014-04-02 18:55:08 +0200

    test folder for automated continous testing

---
+++ tests/runtests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os, sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travis.settings")
+
+
+if __name__ == "__main__":
+    from travis.settings import INSTALLED_APPS
+    from django.core.management import execute_from_command_line
+
+    args = sys.argv
+    args.insert(1, "test")
+
+    for app in INSTALLED_APPS:
+        # include only nodeshot apps
+        if app.startswith('nodeshot.'):
+            args.append(app)
+
+    execute_from_command_line(args)
---
+++ tests/travis/local_settings.example.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'nodeshot_travis',
+        'USER': 'custom_user',
+        'PASSWORD': 'custom_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+DOMAIN = 'yourcustomhost.com'
