79d4af6e817e27351d10a2d2b133e6249c3249a3
stonaz <stefano.tonazzi@gmail.com>
2014-04-16 18:50:33 +0200

    Cleaner output

--- projects/ninux/fabfile.py
+++ projects/ninux/fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import *
 from fabric.contrib.files import append
+from fabric.colors import green
 import random
 import json
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
@@ -19,14 +20,11 @@ def initialize():
     for install_dir in install_dirs:
         if install_dir not in globals():
             initialize_dirs()
-    try:
-        server_name
-    except:
-        initialize_server()
 
 def initialize_server():
-    global server_name
-    server_name = prompt('Server name: ')
+    if 'server_name' not in globals():
+        global server_name
+        server_name = prompt('Server name: ')
        
 def initialize_db():
     db_params = ('db_user','db_pass')
@@ -52,19 +50,20 @@ def uninstall():
     
 def install():
     initialize()
+    initialize_server()
     initialize_db()
+    install_git()
     clone()
     install_dependencies()
     create_virtual_env()
     install_requirements()
     create_db()
     nginx_config()
-    uwsgi_config()
     supervisor_config()
     redis_install()
-    # TODO settings customization
-    # sync_data() # Fails if settings are not correctly set
-    
+    create_settings()
+    sync_data() # Fails if settings are not correctly set
+    start_server()
 
 def update():
     initialize()
@@ -74,24 +73,27 @@ def update():
       
 def clone():
     initialize()
-    with settings(warn_only='true'):
+    print(green("Cloning repository..."))
+    with hide('stdout', 'stderr'):
         run('mkdir -p  %s' % root_dir)
         with cd (root_dir):
             run('git clone %s nodeshot' % git_repo  )
         with cd (deploy_dir):
-            run ('git checkout deploy_test')
-        #pull()
-        #create_virtual_env()
-        #install_requirements()
+            run ('git checkout deploy_test') # to be removed when merged into master
+
+def install_git():
+    print(green("Installing Git..."))
+    with hide('stdout', 'stderr'):
+        run('apt-get -y install git-core')
         
 def install_dependencies():
     initialize()
-    # Next line to be purged and file should be in repository
-    #run ('cp /var/www/nodeshot_deploy/dependencies.txt %s' % project_dir )
-    with cd(project_dir):
-        run('cat dependencies.txt | xargs apt-get -y install')
-    with cd('/tmp'):
-        run('cp %s/install* . && ./install_GEOS.sh && ./install_Postgis.sh' % project_dir )
+    print(green("Installing required packages. This may take a while..."))
+    with hide( 'stdout', 'stderr'):
+        with cd(project_dir):
+            run('cat dependencies.txt | xargs apt-get -y install')
+        with cd('/tmp'):
+            run('cp %s/install* . && ./install_GEOS.sh && ./install_Postgis.sh' % project_dir )
 
 def pull():
     initialize()
@@ -100,29 +102,36 @@ def pull():
         run('git pull')
 
 def create_virtual_env():
-    with cd (project_dir):
-        run('virtualenv python')
+    print(green("Creating virtual env..."))
+    with hide( 'stdout', 'stderr'):
+        with cd (project_dir):
+            run('virtualenv python')
 
 def install_requirements():
     initialize()
-    virtual_env = 'source python/bin/activate'
-    pip_command = 'python/bin/pip install -r %srequirements.txt' % deploy_dir
-    distribute_command = 'python/bin/pip install -U distribute'
-    with cd (project_dir):
-        run( virtual_env + ' &&  ' + pip_command  + ' &&  ' + distribute_command)
+    print(green("Installing requirements. This may take a while..."))
+    with hide( 'stdout', 'stderr'):
+        virtual_env = 'source python/bin/activate'
+        pip_command = 'python/bin/pip install -r %srequirements.txt' % deploy_dir
+        distribute_command = 'python/bin/pip install -U distribute'
+        with cd (project_dir):
+            run( virtual_env + ' &&  ' + pip_command  + ' &&  ' + distribute_command)
  
 def create_db():
     initialize_db()
-    run ('su - postgres -c "createdb nodeshot"')
-    run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION hstore;\'"')
-    run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION postgis;\'"')
-    run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION postgis_topology;\'"')
-    run ('su - postgres -c "createuser %s  -R -S -D "'  % db_user)
-    run ('sudo -u postgres psql -U postgres -d postgres -c \"alter user %s with password \'%s\';\"' % (db_user,db_pass))
-    run ('su - postgres -c "psql -c \'GRANT ALL PRIVILEGES ON DATABASE "nodeshot" to %s \'"' % db_user)
-    
+    print(green("Configuring DB..."))
+    with hide( 'stdout', 'stderr'):
+        run ('su - postgres -c "createdb nodeshot"')
+        run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION hstore;\'"')
+        run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION postgis;\'"')
+        run ('su - postgres -c "psql nodeshot -c \'CREATE EXTENSION postgis_topology;\'"')
+        run ('su - postgres -c "createuser %s  -R -S -D "'  % db_user)
+        run ('sudo -u postgres psql -U postgres -d postgres -c \"alter user %s with password \'%s\';\"' % (db_user,db_pass))
+        run ('su - postgres -c "psql -c \'GRANT ALL PRIVILEGES ON DATABASE "nodeshot" to %s \'"' % db_user)
+        
 def sync_data():
     initialize()
+    print(green("Initializing Nodeshot..."))
     virtual_env = 'source python/bin/activate'
     sync_command = 'python manage.py syncdb && python manage.py migrate && python manage.py collectstatic'
     with cd (project_dir):
@@ -130,13 +139,14 @@ def sync_data():
     
 def nginx_config():
     initialize()
+    print(green("Configuring Nginx..."))
     nginx_dir = '/etc/nginx/ssl'
-    run ('apt-get -y install nginx-full nginx-common openssl zlib-bin')
     run ('mkdir -p %s' % nginx_dir)
     with cd (nginx_dir):
+        print(green("Insert Certificate details..."))
         run ('openssl req -new -x509 -nodes -out server.crt -keyout server.key')
     run('cp /etc/nginx/uwsgi_params /etc/nginx/sites-available/')
-    run ('mkdir -p /var/www/nodeshot/public_html')
+    #run ('mkdir -p /var/www/nodeshot/public_html')
     
     run ('cp %s/nodeshot.yourdomain.com /etc/nginx/sites-available/nodeshot.yourdomain.com' % project_dir)
 
@@ -144,53 +154,45 @@ def nginx_config():
         run ('sed \'s/nodeshot.yourdomain.com/%s/g\' nodeshot.yourdomain.com > %s' % (server_name,server_name))
         run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' %s ' % (project_dir,server_name))
         run ('ln -s /etc/nginx/sites-available/%s /etc/nginx/sites-enabled/%s' % (server_name,server_name))
-        
-def uwsgi_config():
-    initialize()
-    run ('pip install uwsgi')
-    # Next line to be purged and file should be in repository
-    #run ('cp /var/www/nodeshot_deploy/uwsgi.ini %s' % project_dir)
-    with cd (project_dir):
-        run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' uwsgi.ini ' % project_dir)
     
 def supervisor_config():
     initialize()
-    run('apt-get -y install supervisor')
-    # Next lines to be purged and file should be in repository
-    #run ('cp /var/www/nodeshot_deploy/uwsgi.conf /etc/supervisor/conf.d/uwsgi.conf')
-    #run ('cp /var/www/nodeshot_deploy/celery.conf /etc/supervisor/conf.d/celery.conf')
-    #run ('cp /var/www/nodeshot_deploy/celery-beat.conf /etc/supervisor/conf.d/celery-beat.conf')
-    with cd ('/etc/supervisor/conf.d/'):
-        run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' uwsgi.conf ' % project_dir)
-        run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' celery.conf ' % project_dir)
-        run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' celery-beat.conf ' % project_dir)
-    run('supervisorctl update')
+    print(green("Configuring Supervisor..."))
+    with hide( 'stdout', 'stderr'):
+        run ('pip install uwsgi')
+        with cd (project_dir):
+            run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' uwsgi.ini ' % project_dir)
+        run ('cp %s/uwsgi.conf /etc/supervisor/conf.d/uwsgi.conf' % project_dir)
+        run ('cp %s/celery.conf /etc/supervisor/conf.d/celery.conf' % project_dir)
+        run ('cp %s/celery-beat.conf /etc/supervisor/conf.d/celery-beat.conf' % project_dir)
+        with cd ('/etc/supervisor/conf.d/'):
+            run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' uwsgi.conf ' % project_dir)
+            run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' celery.conf ' % project_dir)
+            run ('sed -i \'s#/var/www/nodeshot/projects/ninux#%s#g\' celery-beat.conf ' % project_dir)
+        run('supervisorctl update')
     
 def redis_install():
     initialize()
-    virtual_env = 'source python/bin/activate'
-    pip_command = 'python/bin/pip install -U celery[redis]'
-    run('add-apt-repository ppa:chris-lea/redis-server')
-    run('apt-get update')
-    run('apt-get -y install redis-server')
-    with cd (project_dir):
-        run( virtual_env + ' &&  ' + pip_command)
-
-def file_send(localpath,remotepath):
-    put(localpath,remotepath,use_sudo=True)
+    print(green("Installing redis..."))
+    with hide( 'stdout', 'stderr'):
+        virtual_env = 'source python/bin/activate'
+        pip_command = 'python/bin/pip install -U celery[redis]'
+        run('add-apt-repository -y ppa:chris-lea/redis-server')
+        run('apt-get -y update')
+        run('apt-get -y install redis-server')
+        with cd (project_dir):
+            run( virtual_env + ' &&  ' + pip_command)
 
 def create_settings():
     initialize()
-    filename = 'local_settings.py'
-    #local_settings_file = open("local_settings.py", "w")
+    initialize_db()
+    initialize_server()
+    print(green("Creating Nodeshot config..."))
     local_settings = "DEBUG = False \n" 
     local_settings += "APP_PATH = '%s' \n" % deploy_dir
     local_settings += "SECRET_KEY = '%s' \n" % secret_key
     local_settings += "DOMAIN = '%s' \n" % server_name
-    try :
-        db_user
-    except:
-        initialize_db()
+    local_settings += "ALLOWED_HOSTS = ['*']\n"
         
     DATABASES = {
     'default': {
@@ -204,18 +206,21 @@ def create_settings():
     }
 
     db_settings = json.dumps(DATABASES)
-    with cd ('%s/ninux' % project_dir):
-        append('local_settings.py', local_settings)
-        append('local_settings.py', 'DATABASES = %s' % db_settings)
+    with hide( 'stdout', 'stderr'):
+        with cd ('%s/ninux' % project_dir):
+            append('local_settings.py', local_settings)
+            append('local_settings.py', 'DATABASES = %s' % db_settings)
+            run('cp production_settings.example.py settings.py')
         
         
 def start_server():
     initialize()
+    print(green("Starting Nodeshot server..."))
     with cd (project_dir):
         run('touch log/ninux.error.log')
         run('chmod 666 log/ninux.error.log')
         run('service nginx restart && supervisorctl restart all')
-        
+    print(green("Nodeshot server started"))   
 
     
 
