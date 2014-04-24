1db5908af6aa59d131f6f4216a7f0dea5c5979e1
stonaz <stefano.tonazzi@gmail.com>
2014-04-24 19:08:57 +0200

    Cleaning of 'projects/ninux'

--- projects/ninux/install_GEOS.sh
+++
@@ -1,8 +0,0 @@
-wget http://download.osgeo.org/geos/geos-3.3.8.tar.bz2 &&
-tar xvfj geos-3.3.8.tar.bz2 &&
-cd geos-3.3.8 &&
-./configure &&
-make &&
-make install &&
-cd .. &&
-rm -rf geos-3.3.8.tar.bz2 geos-3.3.8.tar.bz2
