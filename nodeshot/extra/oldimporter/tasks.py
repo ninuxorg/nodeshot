4ff3d95a8cfe6caf8093e98cc390dab1e1d80d7d
Federico Capoano <nemesis@ninux.org>
2014-05-07 13:12:24 +0200

    oldimporter celery task #156

---
+++ nodeshot/extra/oldimporter/tasks.py
@@ -0,0 +1,10 @@
+from celery import task
+from django.core import management
+
+
+@task()
+def import_old_nodeshot(*args, **kwargs):
+    """
+    runs "python manage.py import_old_nodeshot"
+    """
+    management.call_command('import_old_nodeshot', *args, **kwargs)
