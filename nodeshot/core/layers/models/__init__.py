2c43febc05f223c7e034fc579265001109b7df74
Federico Capoano <nemesis@ninux.org>
2014-05-10 15:49:45 +0200

    layers: now depend on nodeshot.core.nodes

--- nodeshot/core/layers/models/__init__.py
+++ nodeshot/core/layers/models/__init__.py
@@ -1,6 +1,14 @@
+from nodeshot.core.base.utils import check_dependencies
 from layer import Layer
 
+
 __all__ = ['Layer']
+
+
+check_dependencies(
+    dependencies='nodeshot.core.nodes',
+    module='nodeshot.core.layers'
+)
 
 
 # ------ Add relationship to ExtensibleNodeSerializer ------ #
