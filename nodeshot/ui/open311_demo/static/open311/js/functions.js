ninuxorg/nodeshot

commit c2abf650e82f7f4918eb4fe8c569182b6e0470f7

Fixes a bug on Open311 UI Map Legend + some code improvements

nodeshot/ui/open311_demo/static/open311/js/functions.js
@@ -56,37 +56,35 @@ function loadLayers(layers) {
     /*
      * Takes all node of a layer and puts them on map in a Leaflet Clustered group
      */
-    var allLayers = []
     for (var i in layers) {
         var color = layers[i].color;
         var clusterClass = layers[i].slug; //CSS class with same name of the layer
         //Creates a Leaflet cluster group styled with layer's colour
         NS_311.mapClusters[layers[i].slug] = createCluster(clusterClass);
         //Loads nodes in the cluster
-        var newClusterNodes = getData(NS_311.__BASEURL__ + 'open311/requests.json?service_code=node&layer=' + layers[i].slug);
-        //Creates GeoJSON from JSON
-        var GeoJSON = new geojsonColl();
-        for (var n in newClusterNodes) {
+        var url = NS_311.__BASEURL__ + 'open311/requests.json?service_code=node&layer=' + layers[i].slug
+        var open311GeoJSON = new Geojson;
+        $.ajax({
+                url: url,
+                dataType: 'json',
+                 success: function(response){
+                            open311GeoJSON.load(response,"long","lat");
+                            var newClusterLayer = loadNodes(layers[i].slug, open311GeoJSON, NS_311.colors[i]);
+                            NS_311.mapClusters[layers[i].slug].addLayer(newClusterLayer);
+                             //Adds cluster to map
+                            map.addLayer(NS_311.mapClusters[layers[i].slug]);
+                            //Creates map controls for the layer
+                            var newClusterKey = "<span style='color:" + NS_311.colors[i] + "'>" + layers[i].name + "</span>";
+                            overlaymaps[newClusterKey] = NS_311.mapClusters[layers[i].slug];
+                            mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
+                            legend.addTo(map);
+                            
+        
+                 }
+        })
 
-            var feature = new featureConstructor;
-            feature.geometry.coordinates = [newClusterNodes[n].long,
-            newClusterNodes[n].lat]
-            feature.properties = newClusterNodes[n]
-            GeoJSON.features.push(feature)
-
-
-        }
-
-        var newClusterLayer = loadNodes(layers[i].slug, GeoJSON, NS_311.colors[i]);
-        NS_311.mapClusters[layers[i].slug].addLayer(newClusterLayer);
-        //Adds cluster to map
-        map.addLayer(NS_311.mapClusters[layers[i].slug]);
-        //Creates map controls for the layer
-        var newClusterKey = "<span style='color:" + NS_311.colors[i] + "'>" + layers[i].name + "</span>";
-        overlaymaps[newClusterKey] = NS_311.mapClusters[layers[i].slug];
-        allLayers[i] = NS_311.mapClusters[layers[i].slug];
+        
     }
-    return allLayers;
 
 }
 
@@ -96,7 +94,9 @@ function createCluster(clusterClass) {
      * Creates cluster group
      */
     var newCluster = new L.MarkerClusterGroup({
+        
         iconCreateFunction: function (cluster) {
+            
             return L.divIcon({
                 html: cluster.getChildCount(),
                 className: clusterClass,

nodeshot/ui/open311_demo/static/open311/js/main.js
@@ -35,23 +35,6 @@ NS_311.status_colors = {
 //Map initialization
 var map = L.map('map').setView([41.87, 12.49], 8);
 var legend = L.control({position: 'bottomleft'});
-
-legend.onAdd = function (map) {
-    var mapLegend = L.DomUtil.create('div','mapLegend')
-    mapLegend.innerHTML="<div><strong>Legend</strong>"
-    _.each(NS_311.status_colors,function(value, key, list){
-        mapLegend.innerHTML += "<div style='clear:both;min-height:10px;width:100px;'>"
-        mapLegend.innerHTML += "<div class='circle' style='float:left;background-color:"+value+"'></div>"
-        mapLegend.innerHTML += "<div style='padding-left:10px;margin-top:-4px;float:left;'>"+key+" requests <strong>("+ NS_311.markerStatusMap[key].length + ")</strong></div>"
-        mapLegend.innerHTML += "</div>"
-        })
-    mapLegend.innerHTML += "</div>"
-    return mapLegend;
-};
-legend.addTo(map);
-
-
-
 var mapBoxLayer = new L.tileLayer(NS_311.TILESERVER_URL).addTo(map);
 var osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 var googleHybrid = new L.Google('HYBRID');
@@ -68,7 +51,7 @@ var overlaymaps = {};
 
 //Load data from server
 var layers = getData(window.__BASEURL__ + 'layers/'); //layers
-var geojsonlayers = getData(window.__BASEURL__ + 'layers.geojson'); //layers' area
+//var geojsonlayers = getData(window.__BASEURL__ + 'layers.geojson'); //layers' area
 
 for (var i in layers) {
     createlayersCSS(layers[i].slug, NS_311.colors[i]);
@@ -87,10 +70,23 @@ var baseMaps = {
     "Google Hybrid": googleHybrid
 
 };
-var mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
+//var mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
 
 //Populate a select field with Layers
 getLayerListSlug(layers);
+
+legend.onAdd = function (map) {
+    var mapLegend = L.DomUtil.create('div','mapLegend')
+    mapLegend.innerHTML="<div><strong>Legend</strong>"
+    _.each(NS_311.status_colors,function(value, key, list){
+        mapLegend.innerHTML += "<div style='clear:both;min-height:10px;width:100px;'>"
+        mapLegend.innerHTML += "<div class='circle' style='float:left;background-color:"+value+"'></div>"
+        mapLegend.innerHTML += "<div style='padding-left:10px;margin-top:-4px;float:left;'>"+key+" requests <strong>("+ NS_311.markerStatusMap[key].length + ")</strong></div>"
+        mapLegend.innerHTML += "</div>"
+        })
+    mapLegend.innerHTML += "</div>"
+    return mapLegend;
+};
 
 
 

nodeshot/ui/open311_demo/static/open311/js/json2geojson.js
@@ -1,0 +1,22 @@
+function Geojson(json, lng, lat) {
+    var Feature = function () {
+        this.type = "Feature",
+        this.geometry = {
+            "type": "Point",
+        }
+        this.properties = {}
+    }
+    this.type = "FeatureCollection",
+    this.features = [];
+    this.load = function (json, lng, lat) {
+        for (var i in json) {
+            //console.log('test' + i)
+            if (json[i][lng] && json[i][lat]) {
+                var feature = new Feature;
+                feature.geometry.coordinates = [json[i][lng], json[i][lat]]
+                feature.properties = json[i]
+                this.features.push(feature)
+            }
+        }
+    }
+}

nodeshot/ui/open311_demo/static/open311/js/utils.js
@@ -51,7 +51,7 @@ function getAddress(lat,lng) {
     });
 }
 
-//$('#loading').hide(); //initially hide the loading icon
+
 
 $(document).ajaxStart(function () {
     $('#loading').show();
@@ -66,15 +66,15 @@ function convertToSlug(Text) {
         .replace(/ +/g, '-');
 }
 
-function geojsonColl() {
-    this.type="FeatureCollection",
-    this.features= [] };
-    
-function featureConstructor() {
-    this.type="Feature",
-    this.geometry = {"type":"Point",}
-    this.properties = {}
-    };
+//function geojsonColl() {
+//    this.type="FeatureCollection",
+//    this.features= [] };
+//    
+//function featureConstructor() {
+//    this.type="Feature",
+//    this.geometry = {"type":"Point",}
+//    this.properties = {}
+//    };
     
     /* AUTHENTICATION
  * ============== */

nodeshot/ui/open311_demo/templates/open311.html
@@ -308,6 +308,7 @@ Rated <strong><%= rating%>/10 </strong> out of <strong><%= votes %></strong> vot
     $.myproject.STATIC_URL = '{{ STATIC_URL }}';
 </script>
 <script src="{% static 'open311/js/utils.js' %}"></script>
+<script src="{% static 'open311/js/json2geojson.js' %}"></script>
 <script src="{% static 'open311/js/functions.js' %}"></script>
 <script src="{% static 'open311/js/main.js' %}"></script>
 {% block scripts%}

commit eb0bc0bef8cb0d8698fcca4a96e59dcac4585063

Fixes  a bug introduced in c2abf650e8

nodeshot/ui/open311_demo/static/open311/js/functions.js
@@ -52,51 +52,79 @@ function addToList(data) {
     })
 }
 
+//function loadLayers(layers) {
+//    /*
+//     * Takes all node of a layer and puts them on map in a Leaflet Clustered group
+//     */
+//    for (var i in layers) {
+//        var color = layers[i].color;
+//        var clusterClass = layers[i].slug; //CSS class with same name of the layer
+//        //Creates a Leaflet cluster group styled with layer's colour
+//        NS_311.mapClusters[layers[i].slug] = createCluster(clusterClass);
+//        //Loads nodes in the cluster
+//        var url = NS_311.__BASEURL__ + 'open311/requests.json?service_code=node&layer=' + layers[i].slug
+//        var open311GeoJSON = new Geojson;
+//        $.ajax({
+//            url: url,
+//            dataType: 'json',
+//            success: function (response) {
+//                open311GeoJSON.load(response, "long", "lat");
+//                var newClusterLayer = loadNodes(layers[i].slug, open311GeoJSON, NS_311.colors[i]);
+//                NS_311.mapClusters[layers[i].slug].addLayer(newClusterLayer);
+//                //Adds cluster to map
+//                map.addLayer(NS_311.mapClusters[layers[i].slug]);
+//                //Creates map controls for the layer
+//                var newClusterKey = "<span style='color:" + NS_311.colors[i] + "'>" + layers[i].name + "</span>";
+//                //overlaymaps[newClusterKey] = NS_311.mapClusters[layers[i].slug];
+//                //mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
+//                console.log( legend)
+//                legend.addTo(map);
+//
+//
+//            }
+//        })
+//
+//
+//    }
+//    
+//}
+
 function loadLayers(layers) {
     /*
-     * Takes all node of a layer and puts them on map in a Leaflet Clustered group
-     */
+* Takes all node of a layer and puts them on map in a Leaflet Clustered group
+*/
+    var allLayers = []
     for (var i in layers) {
         var color = layers[i].color;
         var clusterClass = layers[i].slug; //CSS class with same name of the layer
         //Creates a Leaflet cluster group styled with layer's colour
         NS_311.mapClusters[layers[i].slug] = createCluster(clusterClass);
         //Loads nodes in the cluster
-        var url = NS_311.__BASEURL__ + 'open311/requests.json?service_code=node&layer=' + layers[i].slug
+        var newClusterNodes = getData(NS_311.__BASEURL__ + 'open311/requests.json?service_code=node&layer=' + layers[i].slug);
+        //Creates GeoJSON from JSON
         var open311GeoJSON = new Geojson;
-        $.ajax({
-                url: url,
-                dataType: 'json',
-                 success: function(response){
-                            open311GeoJSON.load(response,"long","lat");
-                            var newClusterLayer = loadNodes(layers[i].slug, open311GeoJSON, NS_311.colors[i]);
-                            NS_311.mapClusters[layers[i].slug].addLayer(newClusterLayer);
-                             //Adds cluster to map
-                            map.addLayer(NS_311.mapClusters[layers[i].slug]);
-                            //Creates map controls for the layer
-                            var newClusterKey = "<span style='color:" + NS_311.colors[i] + "'>" + layers[i].name + "</span>";
-                            overlaymaps[newClusterKey] = NS_311.mapClusters[layers[i].slug];
-                            mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
-                            legend.addTo(map);
-                            
-        
-                 }
-        })
-
-        
+        open311GeoJSON.load(newClusterNodes, "long", "lat")
+        var newClusterLayer = loadNodes(layers[i].slug, open311GeoJSON, NS_311.colors[i]);
+        NS_311.mapClusters[layers[i].slug].addLayer(newClusterLayer);
+        //Adds cluster to map
+        map.addLayer(NS_311.mapClusters[layers[i].slug]);
+        //Creates map controls for the layer
+        var newClusterKey = "<span style='color:" + NS_311.colors[i] + "'>" + layers[i].name + "</span>";
+        overlaymaps[newClusterKey] = NS_311.mapClusters[layers[i].slug];
+        allLayers[i] = NS_311.mapClusters[layers[i].slug];
     }
+    return allLayers;
 
 }
-
 
 function createCluster(clusterClass) {
     /*
      * Creates cluster group
      */
     var newCluster = new L.MarkerClusterGroup({
-        
+
         iconCreateFunction: function (cluster) {
-            
+
             return L.divIcon({
                 html: cluster.getChildCount(),
                 className: clusterClass,
@@ -237,13 +265,13 @@ function openForm(marker) {
                 processData: false,
                 success: function (returndata) {
                     var circle = L.circleMarker(latlng, {
-                                radius: 8,
-                                fillColor: NS_311.status_colors['open'],
-                                //color: color,
-                                weight: 3,
-                                opacity: 1,
-                                fillOpacity: 0.8
-                            }).addTo(map);
+                        radius: 8,
+                        fillColor: NS_311.status_colors['open'],
+                        //color: color,
+                        weight: 3,
+                        opacity: 1,
+                        fillOpacity: 0.8
+                    }).addTo(map);
                     var newMarker = L.marker(latlng);
                     NS_311.mapClusters[layer_inserted].addLayer(newMarker);
                     NS_311.markerMap[returndata] = newMarker;
@@ -251,7 +279,7 @@ function openForm(marker) {
                     legend.removeFrom(map)
                     legend.addTo(map)
                     popupMessage = "Request has been inserted<br>"
-                    popupMessage += "<strong>Request ID: </strong>"+ returndata
+                    popupMessage += "<strong>Request ID: </strong>" + returndata
                     circle.bindPopup(popupMessage).openPopup();
                     map.panTo(latlng)
                     $('#serviceRequestForm').hide("fast", function () {
@@ -305,14 +333,16 @@ function populateOpen311Div(nodeSlug, create) {
         status: status,
     });
     $(nodeDiv).append(compiledTmpl);
-    $(nodeDiv).on('click',function(){showRequestDetail(requestID,node)});
-    
+    $(nodeDiv).on('click', function () {
+        showRequestDetail(requestID, node)
+    });
+
 
     return (nodeDiv, NS_311.nodeRatingAVG)
 
 }
 
-function showRequestDetail(requestID,node) {
+function showRequestDetail(requestID, node) {
     $('#overlay').fadeIn('fast', function () {
         $('#RequestDetails').show();
         $('#RequestDetails').html('<a class="boxclose"  id="RequestDetailsClose"></a>');
@@ -322,46 +352,45 @@ function showRequestDetail(requestID,node) {
             });
         });
         var tmplMarkup = $('#tmplOpen311RequestBox').html();
-        var compiledTmpl = _.template(tmplMarkup, {
-        });
+        var compiledTmpl = _.template(tmplMarkup, {});
         $("#RequestDetails").append(compiledTmpl);
-    
-    //$("#MainPage").hide();
-    //$("#RequestDetails").show();
-    NS_311.nodeSlug = node.slug
-    NS_311.nodeId = requestID.split("-")[1];
-    NS_311.layerSettings = getData(NS_311.__BASEURL__ + 'layers/' + node.layer_slug + '/participation_settings/');
-    NS_311.nodeSettings = getData(NS_311.__BASEURL__ + 'nodes/' + node.slug + '/participation_settings/');
-    NS_311.nodeParticipation = getData(NS_311.__BASEURL__ + 'nodes/' + node.slug + '/participation/');
-    //getParticipationData()
-    var request = getData(NS_311.__BASEURL__ + 'open311/requests/' + requestID + '.json'); 
-    var tmplMarkup = $('#tmplOpen311Request').html();
-    var compiledTmpl = _.template(tmplMarkup, {
+
+        //$("#MainPage").hide();
+        //$("#RequestDetails").show();
+        NS_311.nodeSlug = node.slug
+        NS_311.nodeId = requestID.split("-")[1];
+        NS_311.layerSettings = getData(NS_311.__BASEURL__ + 'layers/' + node.layer_slug + '/participation_settings/');
+        NS_311.nodeSettings = getData(NS_311.__BASEURL__ + 'nodes/' + node.slug + '/participation_settings/');
+        NS_311.nodeParticipation = getData(NS_311.__BASEURL__ + 'nodes/' + node.slug + '/participation/');
+        //getParticipationData()
+        var request = getData(NS_311.__BASEURL__ + 'open311/requests/' + requestID + '.json');
+        var tmplMarkup = $('#tmplOpen311Request').html();
+        var compiledTmpl = _.template(tmplMarkup, {
             request: request,
             requestID: requestID,
-            
+
         });
-    $("#requestContainer").html('');
-    $("#requestContainer").append(compiledTmpl);
+        $("#requestContainer").html('');
+        $("#requestContainer").append(compiledTmpl);
 
-//Votes
-if (NS_311.nodeSettings.participation_settings.voting_allowed && NS_311.layerSettings.participation_settings.voting_allowed) {
-    //console.log("Votes OK")
-    showVotes(NS_311.nodeParticipation.participation.likes,NS_311.nodeParticipation.participation.dislikes)
-}
+        //Votes
+        if (NS_311.nodeSettings.participation_settings.voting_allowed && NS_311.layerSettings.participation_settings.voting_allowed) {
+            //console.log("Votes OK")
+            showVotes(NS_311.nodeParticipation.participation.likes, NS_311.nodeParticipation.participation.dislikes)
+        }
 
-//Comments       
-if (NS_311.nodeSettings.participation_settings.comments_allowed && NS_311.layerSettings.participation_settings.comments_allowed) {
-    //console.log("Comments OK")
-    showComments(NS_311.nodeSlug,NS_311.nodeParticipation.participation.comment_count); 
-}
+        //Comments       
+        if (NS_311.nodeSettings.participation_settings.comments_allowed && NS_311.layerSettings.participation_settings.comments_allowed) {
+            //console.log("Comments OK")
+            showComments(NS_311.nodeSlug, NS_311.nodeParticipation.participation.comment_count);
+        }
 
-//rating       
-if (NS_311.nodeSettings.participation_settings.rating_allowed && NS_311.layerSettings.participation_settings.rating_allowed) {
-    //console.log("Rating OK")
-    showRating(NS_311.nodeSlug,NS_311.nodeParticipation.participation.rating_avg,NS_311.nodeParticipation.participation.rating_count); 
-}
-})
+        //rating       
+        if (NS_311.nodeSettings.participation_settings.rating_allowed && NS_311.layerSettings.participation_settings.rating_allowed) {
+            //console.log("Rating OK")
+            showRating(NS_311.nodeSlug, NS_311.nodeParticipation.participation.rating_avg, NS_311.nodeParticipation.participation.rating_count);
+        }
+    })
 }
 
 function showMainPage(requestID) {
@@ -518,7 +547,7 @@ function populateRating(nodeID, nodeDiv, nodeRatingAVG) {
     $("#star").raty({
         score: nodeRatingAVG,
         number: 10,
-        width: 250 ,
+        width: 250,
         path: $.myproject.STATIC_URL + 'open311/js/vendor/images',
         click: function (score) {
             var nodeID = NS_311.nodeId;

nodeshot/ui/open311_demo/static/open311/js/main.js
@@ -35,14 +35,14 @@ NS_311.status_colors = {
 //Map initialization
 var map = L.map('map').setView([41.87, 12.49], 8);
 var legend = L.control({position: 'bottomleft'});
-var mapBoxLayer = new L.tileLayer(NS_311.TILESERVER_URL).addTo(map);
-var osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var osmLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+//var mapBoxLayer = new L.tileLayer(NS_311.TILESERVER_URL)
 var googleHybrid = new L.Google('HYBRID');
 var googleMap = new L.Google('ROADMAP');
 var googleSat = new L.Google();
 
 //OSM layer added to map
-mapBoxLayer.addTo(map);
+//mapBoxLayer.addTo(map);
 map.on('click', onMapClick);
 var popup = L.popup();
 
@@ -63,18 +63,14 @@ var mapLayersNodes = loadLayers(layers);
 
 //Map Controls
 var baseMaps = {
-    "MapBox": mapBoxLayer,
     "OpenStreetMap": osmLayer,
+ //   "MapBox": mapBoxLayer,
     "Google Sat": googleSat,
     "Google Map": googleMap,
     "Google Hybrid": googleHybrid
 
 };
 //var mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
-
-//Populate a select field with Layers
-getLayerListSlug(layers);
-
 legend.onAdd = function (map) {
     var mapLegend = L.DomUtil.create('div','mapLegend')
     mapLegend.innerHTML="<div><strong>Legend</strong>"
@@ -87,6 +83,13 @@ legend.onAdd = function (map) {
     mapLegend.innerHTML += "</div>"
     return mapLegend;
 };
+legend.addTo(map);
+
+var mapControl = L.control.layers(baseMaps, overlaymaps).addTo(map);
+//Populate a select field with Layers
+getLayerListSlug(layers);
+
+
 
 
 