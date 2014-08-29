8e15163975f951d12fb5952bb47a23d1c8dfa4aa
Federico Capoano <nemesis@ninux.org>
2014-08-29 16:43:28 +0200

    nodeshot.core.api: simplified settings #146

---
+++ nodeshot/core/api/settings.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+
+API_PREFIX = getattr(settings, 'NODESHOT_API_PREFIX', 'api/v1/')
+API_APPS_ENABLED = getattr(settings, 'NODESHOT_API_APPS_ENABLED', [
+    'nodeshot.core.nodes',
+    'nodeshot.core.layers',
+    'nodeshot.core.cms',
+    'nodeshot.community.profiles',
+    'nodeshot.community.participation',
+    'nodeshot.community.notifications',
+    'nodeshot.community.mailing',
+    'nodeshot.networking.net',
+    'nodeshot.networking.links',
+    'nodeshot.networking.services',
+    'nodeshot.open311'
+])
--- nodeshot/core/api/urls.py
+++ nodeshot/core/api/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
+from .settings import API_PREFIX, API_APPS_ENABLED
+
 
 urlpatterns = patterns('nodeshot.core.api.views',
-    url(r'^%s$' % settings.NODESHOT['SETTINGS']['API_PREFIX'], 'root_endpoint', name='api_root_endpoint'),
+    url(r'^%s$' % API_PREFIX, 'root_endpoint', name='api_root_endpoint'),
 )
 
 if 'rest_framework_swagger' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
-        url(r'^%sdocs/' % settings.NODESHOT['SETTINGS']['API_PREFIX'], include('rest_framework_swagger.urls'))
+        url(r'^%sdocs/' % API_PREFIX, include('rest_framework_swagger.urls'))
     )
 
-# loop over all the strings listed in settings.NODESHOT['API']['APPS_ENABLED]
-for app_path in settings.NODESHOT['API']['APPS_ENABLED']:
+# loop over all the strings listed in API_APPS_ENABLED
+for app_path in API_APPS_ENABLED:
     
     # enable only API modules listed in INSTALLED_APPS
     if app_path not in settings.INSTALLED_APPS:
@@ -23,5 +25,5 @@ for app_path in settings.NODESHOT['API']['APPS_ENABLED']:
     module_path = '%s.urls' % app_path
     
     urlpatterns += patterns('',
-        url(r'^%s' % settings.NODESHOT['SETTINGS']['API_PREFIX'], include(module_path))
+        url(r'^%s' % API_PREFIX, include(module_path))
     )
