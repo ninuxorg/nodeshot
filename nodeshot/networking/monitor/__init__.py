f923c449374eca030173ca4be735435466953f26
Federico Capoano <nemesis@ninux.org>
2014-09-02 18:01:45 +0200

    Removed empty TODO apps

--- nodeshot/networking/monitor/__init__.py
+++
@@ -1,5 +0,0 @@
-# this app is dependant on "net"
-from django.conf import settings
-if 'nodeshot.networking.net' not in settings.INSTALLED_APPS:
-    from nodeshot.core.base.exceptions import DependencyError
-    raise DependencyError('nodeshot.networking.monitor depends on nodeshot.networking.net, which should be in settings.INSTALLED_APPS')
