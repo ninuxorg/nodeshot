25089f1d2bf1e30d933b91ce247f4dec1600aaf9
Federico Capoano <nemesis@ninux.org>
2014-09-02 18:00:46 +0200

    nodeshot.ui.open311: simplified settings #146

--- nodeshot/ui/open311_demo/views.py
+++ nodeshot/ui/open311_demo/views.py
@@ -1,10 +1,9 @@
-from django.conf import settings
 from django.shortcuts import render
+from nodeshot.ui.default.settings import TILESERVER_URL
+
 
 def open311(request):
     context = {
-        'TILESERVER_URL': settings.NODESHOT['SETTINGS'].get('TILESERVER_URL', '//a.tiles.mapbox.com/v3/nodeshot-cineca.i6kgg4hb/{z}/{x}/{y}.png')
+        'TILESERVER_URL': TILESERVER_URL
     }
     return render(request,'open311/index.html', context)
-
-
