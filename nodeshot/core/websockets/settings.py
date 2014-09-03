661b52ffdcd69ec46beb9d0bbd40f1d8c64d1b1e
Federico Capoano <nemesis@ninux.org>
2014-09-03 11:40:21 +0200

    nodeshot.core.websockets settings minor fix #146

--- nodeshot/core/websockets/settings.py
+++ nodeshot/core/websockets/settings.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 PUBLIC_PIPE = getattr(settings, 'NODESHOT_WEBSOCKETS_PUBLIC_PIPE', '%s/nodeshot.websockets.public' % os.path.dirname(settings.SITE_ROOT))
 PRIVATE_PIPE = getattr(settings, 'NODESHOT_WEBSOCKETS_PRIVATE_PIPE', '%s/nodeshot.websockets.private' % os.path.dirname(settings.SITE_ROOT))
-DOMAIN = settings.SITE_DOMAIN
+DOMAIN = settings.DOMAIN
 PATH = getattr(settings, 'NODESHOT_WEBSOCKETS_PATH', '')
 LISTENING_ADDRESS = getattr(settings, 'NODESHOT_WEBSOCKETS_LISTENING_ADDRESS', '0.0.0.0')
 LISTENING_PORT = getattr(settings, 'NODESHOT_WEBSOCKETS_LISTENING_PORT', 8080)
