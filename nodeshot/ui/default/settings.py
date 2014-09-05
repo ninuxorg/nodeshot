4716fadc841fd8e81b43578c237d9c5faf78d5f8
Federico Capoano <nemesis@ninux.org>
2014-09-05 15:32:21 +0200

    Switched default tiles to HTTP

--- nodeshot/ui/default/settings.py
+++ nodeshot/ui/default/settings.py
@@ -3,5 +3,5 @@ from django.conf import settings
 from nodeshot.core.base.settings import ADMIN_MAP_COORDINATES as MAP_CENTER
 
 
-TILESERVER_URL = getattr(settings, 'NODESHOT_UI_TILESERVER_URL', '//otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png')
+TILESERVER_URL = getattr(settings, 'NODESHOT_UI_TILESERVER_URL', 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png')
 MAP_ZOOM = getattr(settings, 'NODESHOT_FRONTEND_MAP_ZOOM', 4)
