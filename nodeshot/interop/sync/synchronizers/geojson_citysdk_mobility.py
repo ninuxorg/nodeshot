5b900b577ad9421b5889e542b5475c58c37a2ae4
Federico Capoano <nemesis@ninux.org>
2014-10-01 17:21:16 +0200

    Moved CitySDK sync in separate repo: https://github.com/nemesisdesign/nodeshot-citysdk-synchronizers #178

--- nodeshot/interop/sync/synchronizers/geojson_citysdk_mobility.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from nodeshot.interop.sync.synchronizers import GeoJson
-from .citysdk_mobility import CitySdkMobilityMixin
-
-
-class GeoJsonCitySdkMobility(CitySdkMobilityMixin, GeoJson):
-    """ Import GeoJson and sync CitySDK """
-    SCHEMA = CitySdkMobilityMixin.SCHEMA + GeoJson.SCHEMA
