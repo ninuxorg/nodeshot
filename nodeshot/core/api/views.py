d2b4eddf2af0bbfb18251974741915bf2f8c7837
Federico Capoano <nemesis@ninux.org>
2014-10-03 14:02:21 +0200

    nodeshot.core.api.views cleanup #144

--- nodeshot/core/api/views.py
+++ nodeshot/core/api/views.py
@@ -13,28 +13,22 @@ def root_endpoint(request, format=None):
     List of all the available resources of this RESTful API.
     """
     endpoints = []
-    
     # loop over url modules
     for urlmodule in urlpatterns:
-        
         # is it a urlconf module?
-        try:
-            urlmodule.urlconf_module
+        if hasattr(urlmodule, 'urlconf_module'):
             is_urlconf_module = True
-        except AttributeError:
+        else:
             is_urlconf_module = False
-        
+            
         # if url is really a urlmodule
         if is_urlconf_module:
-            
             # loop over urls of that module
             for url in urlmodule.urlconf_module.urlpatterns:
-                
                 # TODO: configurable skip url in settings
                 # skip api-docs url
                 if url.name in ['django.swagger.resources.view']:
                     continue
-                
                 # try adding url to list of urls to show
                 try:
                     endpoints.append({
@@ -44,6 +38,5 @@ def root_endpoint(request, format=None):
                 # urls of object details will fail silently (eg: /nodes/<slug>/)
                 except NoReverseMatch:
                     pass
-                
-    
+
     return Response(endpoints)
