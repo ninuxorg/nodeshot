9a0ef3c7d1e5278709d43a49d352a77d04b720e6
Federico Capoano <nemesis@ninux.org>
2014-11-26 17:16:22 +0100

    UI: voting (like/dislike) optional #198

--- nodeshot/ui/default/settings.py
+++ nodeshot/ui/default/settings.py
@@ -1,6 +1,15 @@
+import json
+from django.conf import settings
 from leaflet import app_settings as LEAFLET_SETTINGS
 
 
 TILESERVER_URL = LEAFLET_SETTINGS['TILES'][0][1]
 MAP_ZOOM = LEAFLET_SETTINGS['DEFAULT_ZOOM']
 MAP_CENTER = list(LEAFLET_SETTINGS['DEFAULT_CENTER'])
+
+if 'nodeshot.community.participation' in settings.INSTALLED_APPS:
+    VOTING_ENABLED = getattr(settings, 'NODESHOT_UI_VOTING_ENABLED', True)
+else:
+    VOTING_ENABLED = False
+
+VOTING_ENABLED = json.dumps(VOTING_ENABLED)
--- nodeshot/ui/default/views.py
+++ nodeshot/ui/default/views.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.shortcuts import render
-
+from django.conf import settings
 from nodeshot.core.layers.models import Layer
-
-from .settings import TILESERVER_URL, MAP_CENTER, MAP_ZOOM
+from . import settings as ui_settings
 
 if 'nodeshot.core.websockets' in settings.INSTALLED_APPS:
     from nodeshot.core.websockets import DOMAIN, PATH, PORT
@@ -27,8 +25,9 @@ def index(request):
         'layers': layers,
         'layers_allowing_new_nodes': layers_allowing_new_nodes,
         'WEBSOCKETS': WEBSOCKETS,
-        'TILESERVER_URL': TILESERVER_URL,
-        'MAP_CENTER': MAP_CENTER,
-        'MAP_ZOOM': MAP_ZOOM
+        'TILESERVER_URL': ui_settings.TILESERVER_URL,
+        'MAP_CENTER': ui_settings.MAP_CENTER,
+        'MAP_ZOOM': ui_settings.MAP_ZOOM,
+        'VOTING_ENABLED': ui_settings.VOTING_ENABLED
     }
     return render(request, 'index.html', context)
