7cd63037178e1a4fef5d53cd2d179dfd24699985
Federico Capoano <nemesis@ninux.org>
2015-01-16 15:10:08 +0100

    conf.settings: rename redis_cache to django_redis

--- nodeshot/conf/settings.py
+++ nodeshot/conf/settings.py
@@ -156,10 +156,10 @@ if DEBUG:
 else:
     CACHES = {
         'default': {
-            'BACKEND': 'redis_cache.cache.RedisCache',
+            'BACKEND': 'django_redis.cache.RedisCache',
             'LOCATION': '127.0.0.1:6379:1',
             'OPTIONS': {
-                'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             }
         }
     }
