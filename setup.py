9759fff8317eb9e94a37287a2249dcd567e44c0b
Federico Capoano <nemesis@ninux.org>
2015-05-30 21:45:52 +0200

    Removed accidental executable flags

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 from nodeshot import get_version
 
@@ -7,15 +8,12 @@ def get_install_requires():
     parse requirements.txt, ignore links, exclude comments
     """
     requirements = []
-
     for line in open('requirements.txt').readlines():
         # skip to next iteration if comment or empty line
         if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
             continue
-
         # add line to requirements
         requirements.append(line)
-
     return requirements
 
 
