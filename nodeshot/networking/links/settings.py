de76711f7a280f6b2cec8993dda78c1e15fde95d
Federico Capoano <nemesis@ninux.org>
2015-07-08 15:57:20 +0200

    [networking.links] updated netdiff to 0.4.3

--- nodeshot/networking/links/settings.py
+++ nodeshot/networking/links/settings.py
@@ -3,14 +3,13 @@ from datetime import timedelta
 
 DEFAULT_PARSERS = [
     ('netdiff.OlsrParser', 'OLSRd (txtinfo/jsoninfo)'),
-    ('netdiff.BatmanParser', 'batman-advanced (alfred-vis)'),
+    ('netdiff.BatmanParser', 'batman-advanced (alfred-vis/txtinfo)'),
     ('netdiff.BmxParser', 'BMX6 (q6m)'),
     ('netdiff.NetJsonParser', 'NetJSON NetworkGraph'),
     ('netdiff.CnmlParser', 'CNML'),
 ]
 
 PARSERS = DEFAULT_PARSERS + getattr(settings, 'NODESHOT_NETDIFF_PARSERS', [])
-
 TOPOLOGY_UPDATE_INTERVAL = getattr(settings, 'NODESHOT_TOPOLOGY_UPDATE_INTERVAL', 3)
 
 settings.CELERYBEAT_SCHEDULE.update({
