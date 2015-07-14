2c7223d0253ae0a446c5fcf29c178959894fae49
Claudio Mignanti <c.mignanti@unidata.it>
2015-07-14 16:28:18 +0200

    docker: create an initial admin account

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -8,7 +8,8 @@ sleep 3
 if [ ! -f /app/first_start ]; then
     python manage.py migrate --noinput --no-initial-data
     python manage.py loaddata initial_data
-
+    echo "from nodeshot.community.profiles.models import Profile;\
+       Profile.objects.create_superuser('admin', '', 'admin')" | ./manage.py shell
     touch /app/first_start
 fi
 
