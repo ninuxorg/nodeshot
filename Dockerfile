b366b374d9d4efd69509c84c1b4fb8be0221eeb7
Claudio Mignanti <c.mignanti@unidata.it>
2015-07-14 17:06:48 +0200

    docker: use uwsgi

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,13 @@ WORKDIR /app
 RUN apt-get update && apt-get install -yy libgeos-dev
 ADD requirements.txt /app/
 
-RUN pip install -U pip wheel && pip install -r requirements.txt
+RUN pip install -U pip wheel uwsgi && pip install -r requirements.txt
 
 #copy all files
 ADD . /app
 COPY docker/ /app/
 
-EXPOSE 8000
+EXPOSE 3031
 
 RUN mkdir /log && apt-get install -yy redis-server
 
--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -8,10 +8,11 @@ sleep 3
 if [ ! -f /app/first_start ]; then
     python manage.py migrate --noinput --no-initial-data
     python manage.py loaddata initial_data
-    python manage.py collectstatic
+    python manage.py collectstatic --noinput
     echo "from nodeshot.community.profiles.models import Profile;\
        Profile.objects.create_superuser('admin', '', 'admin')" | ./manage.py shell
     touch /app/first_start
 fi
 
-python manage.py runserver 0.0.0.0:8000
+uwsgi --socket 127.0.0.1:3031 --chdir /app \
+    --wsgi-file wsgi.py --master --processes 4 --threads 2
