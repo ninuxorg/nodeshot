6373e237f34f96376240d73a6243341e96473aef
Federico Capoano <nemesis@ninux.org>
2015-09-11 12:30:55 +0200

    Avoid wsgi load error when sentry is enabled
    This change was made necessary with changes in django 1.8

--- nodeshot/conf/wsgi.py
+++ nodeshot/conf/wsgi.py
@@ -3,15 +3,12 @@
 # setting points here.
 
 from django.conf import settings
+from django.core.wsgi import get_wsgi_application
 
+application = get_wsgi_application()
 
 # sentry WSGI handler if configured
 # more info here: http://raven.readthedocs.org/en/latest/config/django.html#wsgi-middleware
 if settings.SENTRY_ENABLED:
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    from django.core.handlers.wsgi import WSGIHandler
-    application = Sentry(WSGIHandler())
-# django default wsgi
-else:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
+    application = Sentry(application)
